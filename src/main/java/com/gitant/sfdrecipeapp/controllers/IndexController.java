2dba5a7d0b1ce47cd5d0b2651d14cd08c1b5d865
Anton Dyakov <dyakant@gmail.com>
2022-12-15 14:36:01 +0300

    create index page controller

---
+++ src/main/java/com/gitant/sfdrecipeapp/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.gitant.sfdrecipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Anton Dyakov on 15.12.2022
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage(){
+        return "index";
+    }
+}
