7f5cb18a2f40ae18a55790adddcdb9ea377404e3
Anton Dyakov <dyakant@gmail.com>
2022-12-20 00:12:12 +0300

    Add UnitOfMeasure entity and relation with Ingredient as OneToOne

---
+++ src/main/java/com/gitant/sfdrecipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,39 @@
+package com.gitant.sfdrecipeapp.domain;
+
+import jakarta.persistence.*;
+
+/**
+ * Created by Anton Dyakov on 20.12.2022
+ */
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String uom;
+
+    @OneToOne
+    private Ingredient ingredient;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUom() {
+        return uom;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+}
